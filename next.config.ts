aefb8e6c810cfe503f7751fe9b4975c3d7b42dd4
ktp-nip-ha <hinta0601@gmai.com>
2026-03-11 12:18:59 +0900

    GitHub Pages公開設定を追加

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export', // 【注釈】これを入れると、Webサイト用の「静的なファイル」として書き出されます
+  images: {
+    unoptimized: true, // 【注釈】GitHub Pagesで画像を表示させるための設定です
+  },
 };
 
 export default nextConfig;
